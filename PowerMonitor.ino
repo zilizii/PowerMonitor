dbccdbf07f266c2ef33bfb24deaaae94ea74af37
zilizii <istvan.zilizi@outlook.com>
2017-02-25 14:39:57 +0100

    Update PowerMonitor.ino
    first debug version use the Serial instead of OLED...
    Power is not cumulative yet.....

--- PowerMonitor.ino
+++ PowerMonitor.ino
@@ -50,5 +50,32 @@ void setup() {
 
 void loop() {
   // Loop starts
+  Serial.print("raw shunt voltage: ");
+  Serial.println(monitor.shuntVoltageRaw());
   
+  Serial.print("raw bus voltage:   ");
+  Serial.println(monitor.busVoltageRaw());
+  
+  Serial.println("--");
+  
+  Serial.print("shunt voltage: ");
+  Serial.print(monitor.shuntVoltage() * 1000, 4);
+  Serial.println(" mV");
+  
+  Serial.print("shunt current: ");
+  Serial.print(monitor.shuntCurrent() * 1000, 4);
+  Serial.println(" mA");
+  
+  Serial.print("bus voltage:   ");
+  Serial.print(monitor.busVoltage(), 4);
+  Serial.println(" V");
+  
+  Serial.print("bus power:     ");
+  Serial.print(monitor.busPower() * 1000, 4);
+  Serial.println(" mW");
+  
+  Serial.println(" ");
+  Serial.println(" ");
+  
+  delay(1000); 
 }
