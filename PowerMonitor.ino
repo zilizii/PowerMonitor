e2c572e2966d06b2385a6dc02fa761a4b400ecbb
zilizii <istvan.zilizi@outlook.com>
2017-02-25 14:35:45 +0100

    Update PowerMonitor.ino
    update with more comments
    Global variable "unsigned long power" added

--- PowerMonitor.ino
+++ PowerMonitor.ino
@@ -25,18 +25,30 @@
 
 INA219 monitor;
 Adafruit_SSD1306 display(OLED_RESET); 
+unsigned long power;
 
 void setup() {
+  // For Debug only
   Serial.begin(BAUD);
+  
+  // init global variables
+  power = 0;
+  
+  // init and configure the INA219 sensor
   monitor.begin();
   monitor.configure(INA219::RANGE_16V, INA219::GAIN_2_80MV, INA219::ADC_64SAMP, INA219::ADC_64SAMP, INA219::CONT_SH_BUS);
   monitor.calibrate(SHUNT_R, SHUNT_MAX_V, BUS_MAX_V, MAX_CURRENT);
+  
+  // init the OLED
+  // Please do not forget to modify the Adafruit_SSD1306 library for the proper OLED setup
   display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR);
   display.display();
   // erease display 
   display.clearDisplay();
+  
 }
 
 void loop() {
-
+  // Loop starts
+  
 }
